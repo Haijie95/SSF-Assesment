4f194fbc6a6aa2aab0d09bf5e2bb78d07b33ab48
Haijie95 <haijie95@gmail.com>
2023-01-27 09:14:51 +0800

    Initial Commit after task 1

---
+++ src/main/java/haijie/PizzaOrderApp/PizzaOrderAppApplication.java
@@ -0,0 +1,13 @@
+package haijie.PizzaOrderApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaOrderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaOrderAppApplication.class, args);
+	}
+
+}
